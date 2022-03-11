fc8e4c1d6325b2bc6412bac456acc3446e416201
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2022-03-11 14:39:51 -0500

    Create Who_Likes_It.cpp

---
+++ Who_Likes_It.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+string likes(vector<string> names)
+{
+    string liked = " ";
+    if(names.size() == 0){
+      liked = "no one likes this";
+    }
+  else if(names.size() == 1){
+    liked = names [0] + " likes this";
+  }
+  else if(names.size()== 2){
+    liked = names[0] + " and " + names[1] + " like this";
+  }
+  else if(names.size() == 3){
+    liked = names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  }
+  else if(names.size() > 3){
+    liked = names[0] + ", " + names[1] + " and " +  to_string((int)names.size() - 2) + " others like this";
+  }
+  return liked;
+}
