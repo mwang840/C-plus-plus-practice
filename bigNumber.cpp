522b2916f2239efc7d34b4a805f5cd7dc81bc735
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2022-03-11 23:10:33 -0500

    Create bigNumber.cpp

---
+++ bigNumber.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int totalDigits(int value){
+  if(value == 0){
+    return 0;
+  }
+  else{
+    return 1 + totalDigits(value / 10);
+  }
+}
+
+bool narcissistic( int value ){
+  int l = totalDigits(value);
+  int duplicate = value;
+  int sum = 0;
+  
+  
+  while(duplicate){
+    sum+=pow(duplicate % 10, l);
+    duplicate /= 10;
+  }
+  
+  return (value == sum);
+}
