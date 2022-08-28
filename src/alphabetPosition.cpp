087eb16b3098eff5a7c7e51799e7e78d195c462e
Maxwell Wang <maxwang@udel.edu>
2022-08-28 13:57:57 -0400

    alphabets

---
+++ src/alphabetPosition.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+std::string alphabet_position(const std::string &text);
+
+int main(){
+    std::cout<< "Hi\n";
+    return 0;
+}
+
+std::string alphabet_position(const std:: string &text){
+    int alphaValue = 0;
+    for(int i = 0; i < text.capacity; i++){
+        
+    }
+    return "";
+}
