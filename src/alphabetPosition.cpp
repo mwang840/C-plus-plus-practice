8524d974f3789d13a301760af80299202bc2bb26
mwang840 <maxwang@udel.edu>
2022-08-30 08:48:51 -0400

    I have gotten the problem done,now need some spacing fix

--- src/alphabetPosition.cpp
+++ src/alphabetPosition.cpp
@@ -2,16 +2,29 @@
 #include <iostream>
 
 std::string alphabet_position(const std::string &text);
+const int num = 31;
 
 int main(){
-    std::cout<< "Hi\n";
+    std::cout<< alphabet_position("The sunset sets at twelve o' clock.");
     return 0;
 }
 
 std::string alphabet_position(const std:: string &text){
+    //init alphabet value is zero
+    //will be reset each iteration
     int alphaValue = 0;
-    for(int i = 0; i < text.capacity; i++){
-        
+    std::string alpha = "";
+    //loops over the text (const string &str)
+    for(std::string::size_type i = 0; i < text.size(); i++){
+        //If the name is uppercase
+        if(text[i]>= 'A' && text[i] <= 'Z'){
+            alphaValue = (int(text[i]) - 'A') + 1;
+            alpha += std::to_string(alphaValue) + " ";
+        }
+        else if(text[i]>='a' && text[i] <= 'z'){
+            alphaValue = (int(text[i]) - 'a') + 1;
+            alpha += std::to_string(alphaValue) + " ";
+        }
     }
-    return "";
+    return alpha;
 }
